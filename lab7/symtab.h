f13dd7fb8d8829f617050ac853e9ee44c93c7cd5
madmejia <72117050+madmejia@users.noreply.github.com>
2026-04-05 20:15:53 -0600

    Refactor sym_entry and sym_table structures

--- lab7/symtab.h
+++ lab7/symtab.h
@@ -1,36 +1,34 @@
 #ifndef SYMTAB_H
 #define SYMTAB_H
- 
 
+struct typeinfo;
+typedef struct typeinfo *typeptr;
+
+// symbol table entry
 typedef struct sym_entry {
-    char *s;                    
-    char *declared_type;        
-    int   is_const;            
-    struct sym_entry *next;     
+    char *s;
+    typeptr type;
+    struct sym_entry *next;
 } *SymbolTableEntry;
- 
 
+// hash table symbol table
 typedef struct sym_table {
-    int nBuckets;               
-    int nEntries;              
-    struct sym_table *parent;   
-    struct sym_entry **tbl;     
-    const char *name;           
+    int nBuckets;
+    int nEntries;
+    struct sym_table *parent;
+    struct sym_entry **tbl;
+    const char *name;
 } *SymbolTable;
- 
 
-SymbolTable      mksymtab(int nbuckets, const char *name, SymbolTable parent);
-void             freesymtab(SymbolTable st);
- 
-
+//needed func prototypes
+SymbolTable mksymtab(int nbuckets, const char *name, SymbolTable parent);
 SymbolTableEntry insertsym(SymbolTable st, const char *s);
 SymbolTableEntry lookupsym(SymbolTable st, const char *s);
-SymbolTableEntry lookup_current_scope(SymbolTable st, const char *s);
- 
+void printsymtab(SymbolTable st, int indent);
+void freesymtab(SymbolTable st);
 
-void printsymtab(SymbolTable st);
- 
+SymbolTableEntry lookup_current_scope(SymbolTable st, const char *s);
 
 int hash(SymbolTable st, char *s);
- 
+
 #endif
