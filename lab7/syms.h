55effa04a77a625f479ec51c8976566ffec24239
madmejia <72117050+madmejia@users.noreply.github.com>
2026-04-05 20:20:54 -0600

    Refactor syms.h to improve declarations and formatting

--- lab7/syms.h
+++ lab7/syms.h
@@ -1,14 +1,16 @@
 #ifndef SYMS_H
 #define SYMS_H
- 
+
 #include "tree.h"
 #include "symtab.h"
-void printsyms(struct tree *t);
-SymbolTable buildsymtabs(struct tree *root, const char *filename);
-void printsymtabs(void);
-void freesymtabs(void);
 
 extern int g_semantic_errors;
+
+void printsyms(struct tree *t);
+SymbolTable buildsymtabs(struct tree *root, const char *filename);
 void check_undeclared(struct tree *root, SymbolTable global);
- 
-#endif 
+void printsymtabs(void);
+void freesymtabs(void);
+void annotate_expr_types(struct tree *root, SymbolTable global);
+
+#endif
