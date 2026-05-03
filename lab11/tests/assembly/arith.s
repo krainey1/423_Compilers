10f34816c8ae1fb29960164395b57550bd87a129
madmejia <madisonvmejia@gmail.com>
2026-05-03 16:32:00 -0600

    adding more comments

--- lab11/tests/assembly/arith.s
+++ lab11/tests/assembly/arith.s
@@ -23,20 +23,41 @@ test:
 	movq	%rsp, %rbp
 	.seh_setframe	%rbp, 0
 	.seh_endprologue
+	#all stuff above is kinda the same general stuff for test.s and hello.s which has already been shown
+	# z = x _ y (OP_ADD INT and global var)
+	# TAC: t1 = x+y; z = t1
 	movl	x(%rip), %edx
 	movl	y(%rip), %eax
-	addl	%edx, %eax
-	movl	%eax, z(%rip)
+	addl	%edx, %eax	#%eax = x+y
+				#Linux would do leal (%rdx,%rax), %eax but MSYS2 GCC 15 does addl directly
+	movl	%eax, z(%rip)	#storing result
+	
+	# z = x - y (OPT)SUB_INT and global var
 	movl	x(%rip), %edx
 	movl	y(%rip), %eax
-	subl	%eax, %edx
-	movl	%edx, z(%rip)
+	subl	%eax, %edx	#subtracts eax from edx such that %edx = x-y
+	movl	%edx, z(%rip)	#storing result
+	
+	# z= x*y
 	movl	x(%rip), %edx
 	movl	y(%rip), %eax
-	imull	%edx, %eax
+	imull	%edx, %eax	# signed multiply and store into %eax
+				# divis would need idivl and sign-extension with cltd/cdq
 	movl	%eax, z(%rip)
 	nop
+
+	#func epilogic (TAC: endproc test)
 	popq	%rbp
 	ret
 	.seh_endproc
 	.ident	"GCC: (Rev13, Built by MSYS2 project) 15.2.0"
+# ---
+# summary thing: TAC opcode -> x86-64 mapping observed (global int variables)
+#
+#  OP_GLOBAL x     ->  .globl x / .bss / .align 4 / x: / .space 4
+#  OP_ASSIGN (const)->  movl $val, x(%rip)
+#  OP_ADD_INT      ->  movl a(%rip),%edx / movl b(%rip),%eax / addl %edx,%eax / movl %eax,dst(%rip)
+#  OP_SUB_INT      ->  movl a(%rip),%edx / movl b(%rip),%eax / subl %eax,%edx / movl %edx,dst(%rip)
+#  OP_MUL_INT      ->  movl a(%rip),%edx / movl b(%rip),%eax / imull %edx,%eax / movl %eax,dst(%rip)
+#  OP_PROC         ->  pushq %rbp / movq %rsp,%rbp
+#  OP_ENDPROC      ->  popq %rbp / ret
--- lab11/tests/assembly/hello.s
+++ lab11/tests/assembly/hello.s
@@ -1,23 +1,28 @@
 	.file	"hello.c"
 	.text
-	.section .rdata,"dr"
+	.section .rdata,"dr"	#read only data section in Windows/MSYS2
 .LC0:
-	.ascii "Hello World!\0"
+	.ascii "Hello World!\0"	#gcc on windows uses .ascii for null term string
 	.text
 	.globl	main
-	.def	main;	.scl	2;	.type	32;	.endef
-	.seh_proc	main
-main:
-	pushq	%rbp
-	.seh_pushreg	%rbp
-	movq	%rsp, %rbp
-	.seh_setframe	%rbp, 0
-	subq	$32, %rsp
-	.seh_stackalloc	32
+	.def	main;	.scl	2;	.type	32;	.endef	#Windows COFF debug info which replaces Linux .type main,@function
+	.seh_proc	main					# Windows struc exception handling prologue marker whiich replaces Linux .cfi_startproc
+main:				#function entry label (TAC: proc main)
+	pushq	%rbp		# save old base pointer on stack
+	.seh_pushreg	%rbp	# the SEH annocation for the push above
+	movq	%rsp, %rbp	# set frame pointer as curent stack pointer
+	.seh_setframe	%rbp, 0	# SEH record frame reg and offset
+	subq	$32, %rsp	# that 32 byte shadow space requirement from Windows like in test.s; happens before nay call and also doesn't exist in Linux
+	.seh_stackalloc	32	# SEH annotation for stack alloc
 	.seh_endprologue
-	call	__main
-	leaq	.LC0(%rip), %rax
-	movq	%rax, %rcx
+	call	__main		# Windows intiializes C runtime (mingw startup or in my case UCRT64)
+	leaq	.LC0(%rip), %rax	#load hello world string addr into %rax also %rip-relative addr is addr = current PC + offset
+	movq	%rax, %rcx	# move str pointer into %rcx
+				# Windows and Linux here so it doesn't quite match the linux table
+				# Linux: arg1 -> %rdi, arg2 -> %rsi, arg3 -> %rdx
+				# Windows: arg1 -> %rcx, arg2 -> %rdx, arg3 -> %r8
+				# TACL param "Hello World!" -> movq addr, %rcx (Windows)
+	# rest is all kind of the same with printf and setting return value to 0 along with windows shadow space dealloc
 	call	printf
 	movl	$0, %eax
 	addq	$32, %rsp
