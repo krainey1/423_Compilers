6a40861811436bc2be05e57cb7a1830674d4d53d
madmejia <madisonvmejia@gmail.com>
2026-05-03 16:46:49 -0600

    finished commenting

--- lab11/tests/assembly/calls.s
+++ lab11/tests/assembly/calls.s
@@ -3,19 +3,26 @@
 	.globl	add
 	.def	add;	.scl	2;	.type	32;	.endef
 	.seh_proc	add
-add:
+add:					#TAC: proc add as entry point for functino add
 	pushq	%rbp
 	.seh_pushreg	%rbp
 	movq	%rsp, %rbp
 	.seh_setframe	%rbp, 0
 	.seh_endprologue
+	
+	# other stuff above kinda also the same as before, but below is where Windows calling is different from linux
+	# Windows x64: arg1=%rcx/%ecx, arg2=%rdx/%edx, arg3=%r9d, arg4=%r9d
+	# Linux x86-64: arg1=%rdi/%edi, arg2=%rsi/%esi, arg3=%edx, arg4%ecx
+	# and then it's more of that shadow space stuff for Windows
 	movl	%ecx, 16(%rbp)
 	movl	%edx, 24(%rbp)
-	movl	16(%rbp), %edx
-	movl	24(%rbp), %eax
-	addl	%edx, %eax
+	
+	#return a+b OP_ADD_INT  and OP_RETURN
+	movl	16(%rbp), %edx	#reload a from shadow space
+	movl	24(%rbp), %eax	#reload b also from shadow space
+	addl	%edx, %eax	# matches TAC: OP_RETURN x where result in %eax and return
 	popq	%rbp
-	ret
+	ret			# return and %eax holds that val
 	.seh_endproc
 	.globl	test
 	.def	test;	.scl	2;	.type	32;	.endef
@@ -28,13 +35,36 @@ test:
 	subq	$48, %rsp
 	.seh_stackalloc	48
 	.seh_endprologue
+	
+	# r = add(3,4)
 	movl	$4, %edx
 	movl	$3, %ecx
+	# TAC does OP_PARM argument assignment in reverse order but the registers matter not load order
 	call	add
-	movl	%eax, -4(%rbp)
+	movl	%eax, -4(%rbp)	#store ret val into local var r
+				# TAC: OP_ASSIGN r, res of call
+				# r is at -4(%rbp) which is first local, 4 bytes from frame top
 	nop
 	addq	$48, %rsp
 	popq	%rbp
 	ret
 	.seh_endproc
 	.ident	"GCC: (Rev13, Built by MSYS2 project) 15.2.0"
+# sum stuff: TAC opcode -> x86-64 mapping observed (function calls)
+#
+#  OP_PROC fname   ->  pushq %rbp / movq %rsp,%rbp / subq $N,%rsp
+#  OP_PARM arg1    ->  movl val, %ecx   (Windows) / %edi (Linux)
+#  OP_PARM arg2    ->  movl val, %edx   (Windows) / %esi (Linux)
+#  OP_CALL fname,n ->  call fname  (result lands in %eax)
+#  OP_RETURN x     ->  result already in %eax from addl; then ret
+#  OP_ASSIGN local ->  movl %eax, -N(%rbp)
+#  OP_ENDPROC      ->  addq $N,%rsp / popq %rbp / ret
+#
+#  Windows register order for parameters (differs from Linux but shouldn't matter rlly):
+#    Param#:  1      2      3     4
+#    32-bit: %ecx  %edx   %r8d  %r9d
+#    64-bit: %rcx  %rdx   %r8   %r9
+#
+#  Linux register order (from assignment table):
+#    Param#:  1      2      3     4     5     6
+#             %rdi  %rsi   %rdx  %rcx  %r8   %r9
--- lab11/tests/assembly/control.s
+++ lab11/tests/assembly/control.s
@@ -3,21 +3,51 @@
 	.globl	foo
 	.def	foo;	.scl	2;	.type	32;	.endef
 	.seh_proc	foo
-foo:
+foo:				#TAC: proc foo which is int foo(intx)
 	pushq	%rbp
 	.seh_pushreg	%rbp
 	movq	%rsp, %rbp
 	.seh_setframe	%rbp, 0
 	.seh_endprologue
+
+	#Receive param x
 	movl	%ecx, 16(%rbp)
-	cmpl	$0, 16(%rbp)
-	jle	.L2
-	movl	$1, %eax
-	jmp	.L3
+	#OP_JUMP_FALSE/OP_JUMP_TRUE if (x>0)
+	cmpl	$0, 16(%rbp)	#compare x with 0
+				# also sets condition flags: ZF, SF, OF, etc.
+				# TAC: comparison is x>0 so check if x<= to SKIP
+	jle	.L2		# jump if less than or equal,
+				# TAC: OP_JUMP_FALSE if not x>0 to jle.L2
+	# then branch ret 1
+	movl	$1, %eax	#TAC: OP_ASSIGN result, const(1)
+	jmp	.L3		# TAC: OP_JUMP .L3 (skip L3 by jumping over block)
 .L2:
-	movl	$0, %eax
-.L3:
+	movl	$0, %eax	#else branch falls through to .L3
+.L3:				#TAC: OP_LABLE .L3 shared func exit
 	popq	%rbp
 	ret
 	.seh_endproc
 	.ident	"GCC: (Rev13, Built by MSYS2 project) 15.2.0"
+# another summary thing: TAC opcode -> x86-64 mapping observed (control flow)
+#
+#  if (x > 0) compiles as:
+#    cmpl  $0, x_location     # compare x to 0
+#    jle   .L_else             # jump if condition is FALSE (inverted!)
+#    <then block>
+#    jmp   .L_end              # OP_JUMP: skip else
+#  .L_else:                    # OP_LABEL
+#    <else block>
+#  .L_end:                     # OP_LABEL (follow label)
+#
+#  Impt to note: gcc INVERTS the branch condition and jumps to the ELSE.
+#  So "if x > 0" becomes "jle" (jump if NOT > 0).
+#  Full condition code jump table:
+#    >   (gt)  -> jle to skip   |   <=  (lte) -> jg  to skip
+#    <   (lt)  -> jge to skip   |   >=  (gte) -> jl  to skip
+#    ==  (eq)  -> jne to skip   |   !=  (neq) -> je  to skip
+#
+#  OP_JUMP        ->  jmp .Ln
+#  OP_JUMP_TRUE   ->  cmpq $0, val / jne .Ln   (jump if nonzero = true)
+#  OP_JUMP_FALSE  ->  cmpq $0, val / je  .Ln   (jump if zero = false)
+#  OP_LABEL Ln    ->  .Ln:
+#  OP_RETURN x    ->  movl $x, %eax  (then fall through to epilogue)
